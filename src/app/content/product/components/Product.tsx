ea0c5e1b2d98009fffd60656c3e2b32d007f4d9d
vynguyenn02 <vyhellen2112@gmail.com>
2024-09-18 12:49:33 +0700

    feat: product

---
+++ src/app/content/product/components/Product.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const Product = () => {
+    return (
+    
+     <div className="wrapper">
+            
+           
+            
+        </div>
+   
+       
+    )
+}
+
+export default Product;
